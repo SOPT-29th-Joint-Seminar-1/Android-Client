268daf3eebc1f7766fde371b6b9302064346c7c0
Chan <ieejo716@naver.com>
2021-11-15 22:15:42 +0900

    [FEAT] : ViewPager2와 Indicator Test

---
+++ app/src/main/java/org/sopt/hapdongseminar_29th/View/FragmentAdapter.kt
@@ -0,0 +1,15 @@
+package org.sopt.hapdongseminar_29th.View
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    val fragments = mutableListOf<Fragment>()
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment =
+        fragments[position]
+
+}
