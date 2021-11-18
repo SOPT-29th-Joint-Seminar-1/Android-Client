2892cbdc58c2ffd67900a4604688b54a47fa5127
hyunji <24hyunji@naver.com>
2021-11-18 21:46:58 +0900

    [FEAT] 팝업 3초후 닫기

--- app/src/main/java/org/sopt/hapdongseminar_29th/PopupActivity.kt
+++ app/src/main/java/org/sopt/hapdongseminar_29th/PopupActivity.kt
@@ -4,12 +4,14 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import org.sopt.hapdongseminar_29th.databinding.ActivityPopupBinding
 
 class PopupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPopupBinding
+    var isRunning:Boolean=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +24,31 @@ class PopupActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val autoclose=AutoClose()
+        autoclose.start()
+
 
         setContentView(binding.root)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        isRunning=false
+    }
+
+    inner class AutoClose : Thread(){
+        override fun run() {
+            try{
+                while(isRunning) {
+                    sleep(3000)
+                    isRunning=false
+                    finish()
+                }
+            }catch (e:InterruptedException){
+                Log.d("interrupt","스레드 종료")
+            }
+        }
     }
 }
